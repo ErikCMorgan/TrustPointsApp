ee437a5c4a588c4f9157558942e890c0e73fb24b
Erik Morgan <guitarguy624@gmail.com>
2023-05-27 11:54:10 -0400

    Switching from react native cli to expo

--- src/screens/UserDetailsScreen.js
+++ src/screens/UserDetailsScreen.js
@@ -1,69 +1,66 @@
-import React from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import React from 'react'
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native'
 
 class UserDetailsScreen extends React.Component {
-  state = {
-    name: '',
-    email: '',
-  };
+	state = {
+		name: '',
+		email: '',
+	}
 
-  handleNameChange = text => {
-    this.setState({name: text});
-  };
+	handleNameChange = (text) => {
+		this.setState({ name: text })
+	}
 
-  handleEmailChange = text => {
-    this.setState({email: text});
-  };
+	handleEmailChange = (text) => {
+		this.setState({ email: text })
+	}
 
-  handleSubmit = () => {
-    const {name, email} = this.state;
+	handleSubmit = () => {
+		const { name, email } = this.state
+		console.log(`Name is: ${name}, Email is: ${email}`)
+	}
 
-    // Here, you can do whatever you want with the name and email
-    // For example, you can validate the input and save it to your app's state
-    console.log(`Name is: ${name}, Email is: ${email}`);
-  };
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.title}>User Details</Text>
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.title}>User Details</Text>
+				<TextInput
+					style={styles.input}
+					placeholder="Name"
+					onChangeText={this.handleNameChange}
+				/>
 
-        <TextInput
-          style={styles.input}
-          placeholder="Name"
-          onChangeText={this.handleNameChange}
-        />
+				<TextInput
+					style={styles.input}
+					placeholder="Email"
+					onChangeText={this.handleEmailChange}
+					keyboardType="email-address"
+				/>
 
-        <TextInput
-          style={styles.input}
-          placeholder="Email"
-          onChangeText={this.handleEmailChange}
-          keyboardType="email-address"
-        />
-
-        <Button title="Submit" onPress={this.handleSubmit} />
-      </View>
-    );
-  }
+				<Button title="Submit" onPress={this.handleSubmit} />
+			</View>
+		)
+	}
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    padding: 16,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 16,
-  },
-  input: {
-    height: 40,
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 16,
-    padding: 8,
-  },
-});
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		padding: 16,
+	},
+	title: {
+		fontSize: 24,
+		marginBottom: 16,
+	},
+	input: {
+		height: 40,
+		borderColor: 'gray',
+		borderWidth: 1,
+		marginBottom: 16,
+		padding: 8,
+	},
+})
 
-export default UserDetailsScreen;
+export default UserDetailsScreen
